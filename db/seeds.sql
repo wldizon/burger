0659463d98df62fc7d80d3e473bae5b70124e083
Warren Dizon <wldizon@gmail.com>
2017-08-14 21:26:57 -0700

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Backyard Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hells Kitchen Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Hog Burger');
